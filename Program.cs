6c8ba68fe6173ab3660bfff0067a1573ad5f4d4e
hemely Araujo <hemelyraujo21@gmail.com>
2023-03-31 20:18:26 -0300

    exercicio resolvido

---
+++ Program.cs
@@ -0,0 +1,10 @@
+﻿// See https://aka.ms/new-console-template for more information
+string nome, sobrenome;
+Console.WriteLine("Digite seu nome");
+nome = Console.ReadLine()!;
+
+Console.Write("Digite seu sobrenome");
+sobrenome = Console.ReadLine()!;
+
+Console.WriteLine($"nome completo: {nome} {sobrenome}");
+Console.WriteLine($"nome de catalogo: {sobrenome.ToUpper()}, {nome}");
